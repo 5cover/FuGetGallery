9fd7f6052e1cd7b31109bdc105e321e2d5dcd51d
Scover <bardini.raphael@gmail.com>
2025-03-20 08:02:15 +0100

    temp dir allowed?

--- Data/Database.cs
+++ Data/Database.cs
@@ -17,7 +17,7 @@ namespace FuGetGallery
 
         static Database()
         {
-            var homePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var homePath = "/tmp";
             var dbsPath = Path.Combine(homePath, "Databases");
             if (!Directory.Exists(dbsPath)) {
                 Directory.CreateDirectory(dbsPath);
